4dea189a6738be8d5f80056817af9608b50b6653
perdub <my.emailtounity@gmail.com>
2026-01-01 23:08:27 +0300

    Добавьте файлы проекта.

---
+++ src/serialization/Kessoku.Serialization/Types/ISerialization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Kessoku.Serialization.Types
+{
+    public interface ISerialization
+    {
+        void Serialize(BinaryWriter binaryWriter);
+        void Deserialize(BinaryReader binaryReader);
+    }
+}
---
+++ src/serialization/Kessoku.Serialization/Types/IgnoreMemberAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kessoku.Serialization.Types
+{
+    [System.AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class IgnoreMemberAttribute : Attribute
+    {
+        public IgnoreMemberAttribute()
+        {
+        }
+
+    }
+}
